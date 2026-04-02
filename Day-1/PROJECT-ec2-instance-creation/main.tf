a00c83b46120eb90eeb58fd86041d0a0cb4755d3
sudheeryella <105410710+sudheerde@users.noreply.github.com>
2026-04-02 22:21:35 +0530

    Update main.tf
    terra

--- Day-1/PROJECT-ec2-instance-creation/main.tf
+++ Day-1/PROJECT-ec2-instance-creation/main.tf
@@ -1,8 +1,7 @@
 provider "aws" {
-    region = "ap-south-1"  # Set your desired AWS region
+    region = "ap-south-1"
 }
-
 resource "aws_instance" "example" {
-    ami           = "ami-0f6c99577b93a642b"  # Specify an appropriate AMI ID
-    instance_type = "t2.micro"
+    ami           = "ami-0f6c99577b93a642b"
+    instance_type = "t3.micro"  # Nitro-based, also free-tier eligible
 }
